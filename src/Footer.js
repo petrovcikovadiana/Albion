1741583a68551e5416d9311fa48e6e2be95837d2
Diana Petrovčíková <diana.petrovcikova@greipi.team>
2025-08-02 21:02:32 +0200

    Add support link

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,13 @@
-import React from "react";
+// import React from "react";
 
-function Footer() {
-  return (
-    <div className="bg-[#0e1515] border-t border-white/10">
-      <div className="flex flex-row justify-center text-white items-center text-center font-bolder text-xs p-1">
-        <p>2025</p>
-      </div>
-    </div>
-  );
-}
+// function Footer() {
+//   return (
+//     <div className="bg-[#0e1515] border-t border-white/10">
+//       <div className="flex flex-row justify-center text-white items-center text-center font-bolder text-xs p-1">
+//         <p></p>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Footer;
+// export default Footer;
